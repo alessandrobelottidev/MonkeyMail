7c9389fe6f6a623842ba8616ddd73a97f7a41066
Alessandro Belotti <48164098+alessandrobelottidev@users.noreply.github.com>
2023-04-03 15:33:58 +0200

    Removed test hello controller, created UserController and polished the classes around it: User & UserRepository

--- backend/src/main/java/com/belottidigital/monkeymail/models/User.java
+++ backend/src/main/java/com/belottidigital/monkeymail/models/User.java
@@ -1,5 +1,6 @@
 package com.belottidigital.monkeymail.models;
 
+import jakarta.annotation.Nonnull;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Column;
@@ -12,57 +13,73 @@ public class User {
     @Column(name = "username")
     private String username;
 
+    @Nonnull
     @Column(name = "password")
     private String password;
 
+    @Nonnull
     @Column(name = "email")
     private String email;
 
+    @Nonnull
     @Column(name = "firstname")
     private String firstname;
 
+    @Nonnull
     @Column(name = "lastname")
     private String lastname;
 
-    @Column(name = "role")
-    private String role;
-
     protected User() {}
 
-    public User(String username, String password, String email, String firstname, String lastname, String role) {
+    public User(String username, String password, String email, String firstname, String lastname) {
         this.username = username;
         this.password = password;
         this.email = email;
         this.firstname = firstname;
         this.lastname = lastname;
-        this.role = role;
     }
 
     public String getUsername() {
         return username;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getFirstname() {
         return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
     }
 
     public String getLastname() {
         return lastname;
     }
 
-    public String getRole() {
-        return role;
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
     }
 
     public String toString() {
-        return "User [username=" + username + ", password=" + password + ", email=" + email + ", firstname=" + firstname + ", lastname=" + lastname + ", role=" + role + "]";
+        return "User [username=" + username + ", password=" + password + ", email=" + email + ", firstname=" + firstname + ", lastname=" + lastname + "]";
     }
 }
--- backend/src/main/java/com/belottidigital/monkeymail/models/UserRepository.java
+++ backend/src/main/java/com/belottidigital/monkeymail/models/UserRepository.java
@@ -1,11 +1,9 @@
 package com.belottidigital.monkeymail.models;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<User, String> {
-    List<User> findAll();
-
-    User findByUsername(String username);
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUsername(String username);
 }
