4c6deb34190eefd4d7ad69be65fdc66672686ea8
MaicolMicheli12 <maicolmicheli00@gmail.com>
2023-04-03 15:50:50 +0200

    Added footer and components in landing

--- frontend/src/lib/components/Footer.svelte
+++ frontend/src/lib/components/Footer.svelte
@@ -1,3 +1,25 @@
-<footer>
-    Test teter
+<footer class="text-center text-white" style="background-color: #32AFEC">
+  <div class="container p-16">
+    <div class="">
+      <p class="flex flex-col items-center justify-center">
+        <span class="mr-4 pb-4 text-4xl text-center font-extrabold">Register for free!</span>    
+           
+        <button
+          type="button"
+          class=" border-x-8 border-y-8 border-white bg-white text-sky-400 font-bold"
+          data-te-ripple-init
+          data-te-ripple-color="light">
+          Sign up!
+        </button>
+      </p>
+    </div>
+  </div>
+
+  <div
+    class="p-2 text-center"
+    style="background-color: #FFFFFF">
+    <a class="text-black" href="https://google.com/"
+      >© 2023 Copyright MonkeyMail</a
+    >
+  </div>
 </footer>
--- frontend/src/lib/pages/Landing.svelte
+++ frontend/src/lib/pages/Landing.svelte
@@ -1,3 +1,9 @@
 <svelte:head>
     <title>MonkeyMail</title> 
 </svelte:head>
+
+<script>
+    import Footer from "../components/Footer.svelte";
+</script>
+
+<Footer />
