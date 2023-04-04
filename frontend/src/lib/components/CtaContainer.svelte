556b40ea775189023ba4a6993187705d6ad085ac
MaicolMicheli12 <maicolmicheli00@gmail.com>
2023-04-04 16:37:26 +0200

    perfected CtaContainer

--- frontend/src/lib/components/CtaContainer.svelte
+++ frontend/src/lib/components/CtaContainer.svelte
@@ -1,17 +1,13 @@
-<ctaContainer class="text-center text-white" >
-<div class="container p-16  bg-primary">
-    <div class="">
-      <p class="flex flex-col items-center justify-center">
+<div class="text-center text-white container p-16  bg-primary">
+    <p class="flex flex-col items-center justify-center">  
         <span class="mr-4 pb-4 text-4xl text-center font-extrabold">Register for free!</span>    
-           
+            
         <button
-          type="button"
-          class=" border-x-8 border-y-8 border-white bg-white text-primary font-bold"
-          data-te-ripple-init
-          data-te-ripple-color="light">
-          Sign up!
+            type="button"
+            class=" border-x-8 border-y-8 border-white bg-white text-primary font-bold"
+            data-te-ripple-init
+            data-te-ripple-color="light">
+            Sign up!
         </button>
-      </p>
-    </div>
-  </div>
-</ctaContainer>
+    </p>
+</div>
