88eb6a15dd303419da94c19fc60543cbb904b0fa
LamaPigro <marco.vaccaro14@gmail.com>
2023-04-05 17:46:26 +0200

    Added Features

--- frontend/src/lib/components/Features.svelte
+++ frontend/src/lib/components/Features.svelte
@@ -3,11 +3,11 @@
     import featuresBg from '../../assets/features-bg.png';
 </script>
 
-<div class="flex flex-col bg-white container mx-auto py-16">
+<div class="flex flex-col bg-white container mx-auto py-16 px-6">
     
     <h1 class="mb-8 text-center font-bold text-3xl">Features</h1>
 
-    <div class="grid place-content-center sm:grid-cols-3 gap-6">
+    <div class="grid place-content-center md:grid-cols-3 gap-6">
         <div class="bg-sand pt-8 pb-6 px-6 flex flex-col">
             <div class="pb-4 flex flex-row items-center">
                 <div class="ml-2">
@@ -19,7 +19,7 @@
                 </div>
                 <div class="text-2xl font-bold px-4">Features</div>
             </div>
-            <p class="max-w-xs py-2">Lorem ipsum dolor sit amet. Est corrupti blanditiis eos eligendi fuga aut magnam reiciendis qui minima dicta ut fugit quia sed debitis delectus.</p>
+            <p class="text-lg py-2">Lorem ipsum dolor sit amet. Est corrupti blanditiis eos eligendi fuga aut magnam reiciendis qui minima dicta ut fugit quia sed debitis delectus.</p>
         </div>
         
         <div class="bg-primary pt-8 pb-6 px-6 flex flex-col">
@@ -33,7 +33,7 @@
                 </div>
                 <div class="text-2xl font-bold px-4 text-white">Features</div>
             </div>
-            <p class="max-w-xs py-2 text-white">Lorem ipsum dolor sit amet. Est corrupti blanditiis eos eligendi fuga aut magnam reiciendis qui minima dicta ut fugit quia sed debitis delectus.</p>
+            <p class="text-lg py-2 text-white">Lorem ipsum dolor sit amet. Est corrupti blanditiis eos eligendi fuga aut magnam reiciendis qui minima dicta ut fugit quia sed debitis delectus.</p>
         </div>
 
         <div class="bg-sand pt-8 pb-6 px-6 flex flex-col">
@@ -47,11 +47,11 @@
                 </div>
                 <div class="text-2xl font-bold px-4">Features</div>
             </div>
-            <p class="max-w-xs py-2">Lorem ipsum dolor sit amet. Est corrupti blanditiis eos eligendi fuga aut magnam reiciendis qui minima dicta ut fugit quia sed debitis delectus.</p>
+            <p class="text-lg py-2">Lorem ipsum dolor sit amet. Est corrupti blanditiis eos eligendi fuga aut magnam reiciendis qui minima dicta ut fugit quia sed debitis delectus.</p>
         </div>
 	</div>
 
-    <div class="pt-6 pb-4">
-        <img src={featuresBg} alt="">
+    <div class="pt-6 pb-4 max-sm:hidden">
+        <img class="object-cover w-screen" src={featuresBg} alt="">
     </div>
 </div>
