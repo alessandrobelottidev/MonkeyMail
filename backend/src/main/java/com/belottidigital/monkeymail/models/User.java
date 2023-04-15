dc5a1ea94486d29364433eff0a925e1ea1a9f723
Alessandro Belotti <48164098+alessandrobelottidev@users.noreply.github.com>
2023-04-15 23:27:04 +0200

    Added JWT authentication, Spring Boot Web Security and implemented beans such as securityFilterChain, userDetailsService, authenticationProvider, authenticationManager and passwordEncoder.
    The User now implements UserDetails for automatic Web Security associations.
    
    Installed Lombok for easier development and future refactoring.

--- backend/src/main/java/com/belottidigital/monkeymail/models/User.java
+++ backend/src/main/java/com/belottidigital/monkeymail/models/User.java
@@ -1,14 +1,25 @@
 package com.belottidigital.monkeymail.models;
 
 import jakarta.annotation.Nonnull;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Column;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @Column(name = "username")
     private String username;
@@ -29,7 +40,8 @@ public class User {
     @Column(name = "lastname")
     private String lastname;
 
-    protected User() {}
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     public User(String username, String password, String email, String firstname, String lastname) {
         this.username = username;
@@ -37,6 +49,31 @@ public class User {
         this.email = email;
         this.firstname = firstname;
         this.lastname = lastname;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
     public String getUsername() {
